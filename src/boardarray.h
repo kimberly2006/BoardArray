cc484e3abe2196af2b14d676c5c5361a680ebf9d
kimberly daydayan <kimberly.daydayan@cit.edu>
2025-09-19 13:28:41 +0800

    naa ra changes gamay sa add

--- src/boardarray.h
+++ src/boardarray.h
@@ -16,6 +16,21 @@ class BoardArray : public Board {
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+          //  Entry existing  = array[0-4];
+   /*Algorithm (in order):
+        a. Traverse the array up until the last element
+        b. Compare (using the bool compare() function of Entry) the `entry` to be added to the `existing entry` currently traversed.
+        c. To get the current entry, you can access it via `array[i * sizeof(Entry)]` or `*(array + i * sizeof(Entry)). I will discuss first why is this the case.
+        d. If `entry` to be added is indeed greater than the existing one, `SHIFT RIGHT`
+        e. After shift right, add the new entry to the correct position.
+*/
+    //  if(Entry  compare(array[i])){
+      //}
+
+        if(int i = 0; i < 5; i++){
+            array[i];
+        }
+
             return;
         }
 

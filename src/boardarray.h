ae5f40125b84280ec847721be26382a463ac8a12
Kimberly S. Daydayan <kimberly.daydayan@cit.edu>
2025-09-23 01:46:18 +0800

    dara sakto

--- src/boardarray.h
+++ src/boardarray.h
@@ -16,27 +16,30 @@ class BoardArray : public Board {
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
-<<<<<<< HEAD
-
-
-=======
-          //  Entry existing  = array[0-4];
-   /*Algorithm (in order):
-        a. Traverse the array up until the last element
-        b. Compare (using the bool compare() function of Entry) the `entry` to be added to the `existing entry` currently traversed.
-        c. To get the current entry, you can access it via `array[i * sizeof(Entry)]` or `*(array + i * sizeof(Entry)). I will discuss first why is this the case.
-        d. If `entry` to be added is indeed greater than the existing one, `SHIFT RIGHT`
-        e. After shift right, add the new entry to the correct position.
-*/
-    //  if(Entry  compare(array[i])){
-      //}
-
-        if(int i = 0; i < 5; i++){
-            array[i];
+        int pos = idex;
+        for(int i = 0; i < index; ){
+            if(entry -> compare(&array[i])){
+                pos = i;
+                break;
+            }
+        }
+        if(index < SIZE){
+            for(int j = index; j > pos; --j){
+                array[j] = array[j - 1];
+            }
+            array[pos] = *entry;
+            ++index;
+        }else{
+            if(pos == SIZE){
+                std::cout<<entry->name<<" 's score is too low to be added";
+            }else{
+                for(int j = SIZE - 1; j > pos; --j){
+                    array[j] = array[j - 1];
+                }
+                array[pos] = *entry;
+            }
         }
 
-            return;
->>>>>>> cc484e3abe2196af2b14d676c5c5361a680ebf9d
         }
 
         void print() {
